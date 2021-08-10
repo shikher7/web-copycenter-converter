bb90d3230556b87b0614ab9a66ae7c34f46093ce
slijirqqq <cult6.cult6.cult6@gmail.com>
2021-08-10 17:50:58 +0500

    Checkout current file to later commit

--- BackEnd/submit.py
+++ BackEnd/submit.py
@@ -1,4 +1,39 @@
-import subprocess
+import cups
+import copy
 
-lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
-lpr.stdin.write('your_data_here')
+
+class Printers:
+    __Printers_list = {}
+
+    def __init__(self):
+        self.__connection = cups.Connection()
+        self.__update_printers_list()
+
+    def __update_printers_list(self):
+        self.__Printers_list = copy.deepcopy(self.__connection.getPrinters())
+
+    def get_printers_list(self):
+        return self.__Printers_list
+
+
+class ChoicePrinter(Printers):
+    def __init__(self, printer_name, options_for_printer, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__printer_name = printer_name
+        self.__options = options_for_printer
+        self.__selected_printer = self.get_printers_list()[printer_name]
+
+    def get_selected_printer(self):
+        return self.__selected_printer
+
+    def print_file(self):
+        cups.Connection().printFile(self.__printer_name, '/BackEnd/trash/skannnn_index_26.07.21.pdf', 'lol', options=self.__options)
+
+
+if __name__ == "__main__":
+    options = {"media": "A4",
+               "sides": "two-sided-long-edge",
+               'n': '2'}
+    # options_for_printer = {}
+    obj = ChoicePrinter(printer_name='HP_Deskjet_4510_series_6A3693_', options_for_printer=options)
+    obj.print_file()
